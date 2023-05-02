4dd22f548257da8f139d49851b5c2bfca6d2144a
md al-amin <muhammadalamin809@gmail.com>
2023-05-02 23:19:59 +0800

    single chef data create

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const cors = require('cors')
 
 const app = express();
 const port = 5000;
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('this is server is running')
 })
 app.get('/chef', (req, res) => {
     res.send(chefData)
 })
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const chef = chefData.find(chef => chef.id === id)
+    res.send(chef)
+})
 
-app.use(cors())
+
 
 
 app.listen(port, () => {
